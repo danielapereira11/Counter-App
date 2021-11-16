9d99a8df84962c561896ce676a21fdbae43b2510
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-16 21:48:34 +0000

    Creating main variables
    Creating the main variables considering the main elements on the dom that will be manipulated

---
+++ index.js
@@ -0,0 +1,6 @@
+const countEl = document.querySelector("#count-el");
+const addScoreBtn = document.querySelector("#like-btn");
+const minusScoreBtn = document.querySelector("#dislike-btn");
+const saveBtn = document.querySelector("#save-btn");
+const opinionHeader = document.querySelector("#opinion-header");
+let count = 0;
