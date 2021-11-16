eb4c139ed957282b50ca758591b72a3071e0e5e7
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-16 21:51:48 +0000

    Added like and dislike btn interaction
    Added basic interaction with the like and dislike btns to show the opinion header

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const minusScoreBtn = document.querySelector("#dislike-btn");
 const saveBtn = document.querySelector("#save-btn");
 const opinionHeader = document.querySelector("#opinion-header");
 let count = 0;
+
+function addScore() {
+  opinionHeader.innerHTML = "I like this!";
+}
+
+function removeScore() {
+  opinionHeader.innerHTML = "I don't like this!";
+}
+
+addScoreBtn.addEventListener("click", addScore);
+minusScoreBtn.addEventListener("click", removeScore);
