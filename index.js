d9ff160842b8ae47bab933c54b8eb7e23a011de2
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-17 18:26:12 +0000

    Added negative and positive count values to JS
    Added neg and pos count variables & added them to the removeScore and addScore functions, respectively

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const minusScoreBtn = document.querySelector("#dislike-btn");
 const saveBtn = document.querySelector("#save-btn");
 const clearBtn = document.querySelector("#clear-btn");
 const opinionHeader = document.querySelector("#opinion-header");
+const negCountEl = document.querySelector("#negative-count");
+const posCountEl = document.querySelector("#positive-count");
 let count = 0;
+let negCountVal = 0;
+let posCountVal = 0;
 
 function addScore() {
   opinionHeader.innerHTML = "I like this!";
   count += 1;
   countEl.innerHTML = count;
+  posCountVal += 1;
+  posCountEl.innerHTML = posCountVal;
 }
 
 function removeScore() {
   opinionHeader.innerHTML = "I don't like this!";
   count -= 1;
   countEl.innerHTML = count;
+  negCountVal += 1;
+  negCountEl.innerHTML = negCountVal;
 }
 
 function saveScore() {
@@ -37,3 +45,5 @@ minusScoreBtn.addEventListener("click", removeScore);
 saveBtn.addEventListener("click", saveScore);
 saveBtn.addEventListener("dblclick", consoleScore);
 clearBtn.addEventListener("click", clearScore);
+
+// add a function, that says that when we click on the button for like or dislike, the round number at the bottom flashes once for each click
